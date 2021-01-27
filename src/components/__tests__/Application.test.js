961143ab1b45c8ac36293e8654c032f642a7d9b3
Audrey <audreyuu@gmail.com>
2021-01-26 22:28:14 -0500

    Installed cypress

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -46,32 +46,24 @@ describe("Application", ()=> {
   });
 
   it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
-    // 1. Render the Application.
     const { container } = render(<Application />);
   
-    // 2. Wait until the text "Archie Cohen" is displayed.
     await waitForElement(() => getByText(container, "Archie Cohen"));
-  
-    // 3. Click the "Delete" button on the booked appointment.
+
     const appointment = getAllByTestId(container, "appointment").find(
       appointment => queryByText(appointment, "Archie Cohen")
     );
     
     fireEvent.click(queryByAltText(appointment, "Delete"));
   
-    // 4. Check that the confirmation message is shown.
     expect(getByText(appointment, "Are you sure you would like to delete appointment?"));
   
-    // 5. Click the "Confirm" button on the confirmation.
     fireEvent.click(getByText(appointment, "Confirm"));
   
-    // 6. Check that the element with the text "Deleting" is displayed.
     expect(getByText(appointment, "Deleting")).toBeInTheDocument();
-    
-    // 7. Wait until the element with the "Add" button is displayed.
+
     await waitForElement(()=> queryByAltText(appointment, "Add"));
-  
-    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+
     const day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
     );
