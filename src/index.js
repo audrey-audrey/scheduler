aecc29e34a245611be054c9d1420330e4d1c207d
Audrey <audreyuu@gmail.com>
2021-01-27 22:20:40 -0500

    Configured netlify and axios for deployment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import axios from "axios";
+
 import "index.scss";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 
 import Application from "components/Application";
 
