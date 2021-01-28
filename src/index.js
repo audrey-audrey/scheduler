accd0689b15fe0eeed1b02c800674b2c548f857b
Audrey <audreyuu@gmail.com>
2021-01-27 22:23:52 -0500

    Fixed src/index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import axios from "axios";
 
 import "index.scss";
 
+import Application from "components/Application";
+
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 }
-
-import Application from "components/Application";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
